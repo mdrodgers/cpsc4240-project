8285e87db4855650dd43ac25e12f28f5fde40593
Matthew Rodgers (macbook) <mattmusic1117@gmail.com>
2017-04-11 19:42:15 -0400

    Web server work

--- basic_server/index.js
+++ basic_server/index.js
@@ -1,13 +1,41 @@
 var express = require('express');
+var path = require('path');
+var mysql = require('mysql');
 var app = express();
 var port = 8080;
 
-app.use(express.static(__dirname + "/public"));
+var connection = mysql.createConnection({
+   host: 'localhost',
+   user: 'user??',
+   password: 'password??',
+   database: 'database??'
+});
+connection.connect(function(err) {
+   if(!err) {
+      console.log("Database Connected");
+   } else {
+      console.log("Error in creating database");
+   }
+});
+
+app.use(express.static('public'));
 
 app.listen(port, function() {
-   console.log('Server running');
+   console.log('Server running on ' + port);
 });
 
 app.get('/', function(req, res) {
-   res.sendfile('./public/index.html');
+   res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/api/data', function(req, res) {
+   connection.query("query string", function(err, rows, fields) {
+      connection.end();
+      if(!err) {
+         res.json(rows);
+      } else {
+         res.status(500);
+         res.send("Connection fucked up");
+      }
+   });
 });
