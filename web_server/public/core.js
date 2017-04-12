bae6aa337f8ec707af589d0da17c44fd9cfe8a6a
Matthew Rodgers (macbook) <mattmusic1117@gmail.com>
2017-04-12 13:09:55 -0400

    other charts added

--- web_server/public/core.js
+++ web_server/public/core.js
@@ -1,47 +1,47 @@
 window.onload = function () {
 
    var rawData;
-   var chart = new CanvasJS.Chart("chartContainer");
-   chart.options.title = { text: "CPU usage" };
+   var cpuChart = new CanvasJS.Chart("cpuChartContainer");
+   var diskChart = new CanvasJS.Chart("diskChartContainer");
+   var memoryChart = new CanvasJS.Chart("memoryChartContainer");
+   cpuChart.options.title = { text: "CPU Usage" };
+   diskChart.options.title = { text: "Disk Usage" };
+   memoryChart.options.title = { text: "Memory Usage" };
 
    var xhttp = new XMLHttpRequest();
    xhttp.onreadystatechange = function() {
       if(this.readyState == 4 && this.status == 200) {
          rawData = JSON.parse(xhttp.responseText);
          //Need a bit more to fill in the dataPoints
-         dpoints = [];
+         cpupoints = [];
+         diskpoints = [];
+         memorypoints = [];
          for( key in rawData ) {
             //console.log(rawData[key].time);
-            dpoints.push( { label: rawData[key].time, y: rawData[key].cpu} );
+            cpupoints.push( { label: rawData[key].time, y: rawData[key].cpu} );
+            diskpoints.push( { label: rawData[key].time, y: rawData[key].disk} );
+            memorypoints.push( { label: rawData[key].time, y: rawData[key].memory} );
          }
-         //console.log(dpoints);
+         //console.log(cpupoints);
 
-         chart.options.data = [];
-         chart.options.data[0] = { type: "line", name: "values" };
-         chart.options.data[0].dataPoints = dpoints;
-         chart.render();
+         cpuChart.options.data = [];
+         cpuChart.options.data[0] = { type: "line", name: "values" };
+         cpuChart.options.data[0].dataPoints = cpupoints;
+         cpuChart.render();
+
+         diskChart.options.data = [];
+         diskChart.options.data[0] = { type: "line", name: "values" };
+         diskChart.options.data[0].dataPoints = cpupoints;
+         diskChart.render();
+
+         memoryChart.options.data = [];
+         memoryChart.options.data[0] = { type: "line", name: "values" };
+         memoryChart.options.data[0].dataPoints = cpupoints;
+         memoryChart.render();
       } else {
          console.log("Error in async call");
       }
    };
    xhttp.open("GET", "http://localhost:8080/api/data", true);
    xhttp.send();
-
-/*
-   dpoints = [
-         { label: "a", y: 7},
-         { label: "b", y: 21},
-         { label: "c", y: 3},
-         { label: "d", y: 2},
-         { label: "e", y: 10},
-         { label: "f", y: 5},
-         { label: "g", y: 2}
-   ];
-
-   chart.options.data = [];
-   chart.options.data[0] = { type: "line", name: "values" };
-   chart.options.data[0].dataPoints = dpoints;
-   chart.render();
-*/
-
 }
