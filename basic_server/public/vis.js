f138fc8f60d85b73e7f4c9fd3a2a3d9fb404fd88
Matthew Rodgers (macbook) <mattmusic1117@gmail.com>
2017-04-11 20:37:04 -0400

    added async call

--- basic_server/public/vis.js
+++ basic_server/public/vis.js
@@ -1,43 +1,42 @@
-/*window.onload = function() {
-   var chart = new CanvasJS.Chart("chartContainer", {
-      title:{
-         text:"Chart Title"
-      },
-      data:[
-         {
-            type: "column",
-            dataPoints: [
-               { label: "a", y: 10}
-               { label: "b", y: 12}
-               { label: "c", y: 5}
-               { label: "d", y: 20}
-               { label: "e", y: 23}
-            ]
-         }
-      ]
-   });
-   chart.render();
-}*/
-
 window.onload = function () {
 
-   var chart = new CanvasJS.Chart("chartContainer", {
-      title:{
-         text: "My First Chart in CanvasJS"              
-      },
-      data: [              
-      {
-         // Change type to "doughnut", "line", "splineArea", etc.
-         type: "column",
-         dataPoints: [
-            { label: "apple",  y: 10  },
-            { label: "orange", y: 15  },
-            { label: "banana", y: 25  },
-            { label: "mango",  y: 30  },
-            { label: "grape",  y: 28  }
-         ]
+   var rawData;
+   var chart = new CanvasJS.Chart("chartContainer");
+   chart.options.title = { text: "Chart Title" };
+
+   var xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function() {
+      if(this.readyState == 4 && this.status == 200) {
+         rawData = xhttp.responseText;
+         //Need a bit more to fill in the dataPoints
+         dpoints = [];
+         rawData.forEach(function(val, i) {
+            dpoints.push( { label: i, y: val} );
+         });
+
+         chart.options.data = [];
+         chart.options.data[0] = { type: "line", name: "values" };
+         chart.options.data[0].dataPoints = dpoints;
+         chart.render();
+      } else {
+         console.log("Error in async call");
       }
+   };
+   xhttp.open("GET", "localhost:8080/api/data", true);
+   xhttp.send();
+
+   dpoints = {
+      type: "line",
+      name: "plotted points",
+      dataPoints: [
+         { label: "a", y: 7},
+         { label: "b", y: 21},
+         { label: "c", y: 3},
+         { label: "d", y: 2},
+         { label: "e", y: 10},
+         { label: "f", y: 5},
+         { label: "g", y: 2}
       ]
-   });
-   chart.render();
-}
+   };
+
+   }
