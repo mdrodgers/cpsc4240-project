2002a28062ca10fa171783d0c29e16ded49e2236
ltmorro <ltmorro@clemson.edu>
2017-04-28 21:18:51 -0400

    Added AES encryption to data sent between client and server

--- monitor.py
+++ monitor.py
@@ -6,15 +6,51 @@ from datetime import datetime
 import socket
 import time 
 import sys
+import getpass
+import hashlib
+import base64
+from Crypto import Random
+from Crypto.Cipher import AES 
+
+class AESCipher(object):
+
+    def __init__(self, key): 
+        self.bs = 32
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, raw):
+        raw = self._pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw))
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
+
+
 id=0
 
-if len(sys.argv) < 3:
-	print("Usage ./monitor IP port")
+if len(sys.argv) < 4:
+	print("Usage ./monitor IP port -p")
 	quit()
 tcpIP=sys.argv[1]
 tcpPort=int(sys.argv[2])
+mPass= getpass.getpass()
+
 #continually send computer stats (every 15 seconds)
 while 1:
+	#create new AES cipher
+	mAES= AESCipher(mPass)
 	#record the current time
 	currentTime= str(datetime.now()) 
 
@@ -34,13 +70,15 @@ while 1:
 	#set up the tcp socket and attempt to connect
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.connect((tcpIP, tcpPort))
-	newId=sock.recv(1024).decode()
+	data=sock.recv(1024)
+	newId=mAES.decrypt(data)
 	if id == 0:
 		id=newId
 	#concat message with | as the delimiter
 	message= str(id) + "|" + str(currentTime) + "|" + str(userPostition) + "|" +str(cpuUsage) + "|" + str(memoryUsage) + "|" +str(diskUsage)
-	#encode the bytes and send
-	sock.send(message.encode())
+	#encrypt the bytes and send
+	sendData= mAES.encrypt(message)
+	sock.send(sendData)
 
 	#print the data we sent
 	print(currentTime)
@@ -49,5 +87,5 @@ while 1:
 	print(memoryUsage)
 	print(diskUsage)
 
-	#sleep for 5 minutes
-	time.sleep(15)
+	#sleep for 15 seconds
+	time.sleep(5)
--- monitorServer.py
+++ monitorServer.py
@@ -3,11 +3,47 @@ import socket
 import mysql.connector
 from datetime import date
 import sys
+import getpass
+import hashlib
+import base64
+from Crypto import Random
+from Crypto.Cipher import AES 
+
+class AESCipher(object):
+
+    def __init__(self, key): 
+        self.bs = 32
+        self.key = hashlib.sha256(key.encode()).digest()
+        
+
+    def encrypt(self, raw):
+        raw = self._pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw))
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
+
 
 #temp localhost ip for testing
-if len(sys.argv) < 2:
-	print("Usage ./monitorServer port")
+if len(sys.argv) < 5:
+	print("Usage ./monitorServer <port> -u <user> -p")
 	quit()
+mPass= getpass.getpass()
+
+
+username= sys.argv[3]
 udpIP = "127.0.0.1"
 udpPort = int(sys.argv[1])
 #set up the tcp socket and bind, wait for incoming connections
@@ -17,50 +53,59 @@ sock.listen(1)
 
 #continually listen for new data
 while 1:
+	mAES= AESCipher(mPass)
 	#accept new connections and print ip address
 	conn, addr = sock.accept()
-	print("New connection from ", addr)
-	db = mysql.connector.connect(user="root", password="password", db="statsdb")
+	print("New connection from %s" % addr[0])
+	try:
+		db = mysql.connector.connect(user=username, password=mPass, db="statsdb")
+		
+	except Exception, e:
+		print("Incorrect MySQL password. Exiting.")
+		exit()
 	cur= db.cursor(buffered=True)
-	
 	#find the largest ID so far, set to 1 larger
 	query= ("SELECT MAX(id),ip FROM stats")
+	
 	cur.execute(query)
 	row=cur.fetchone()
 	if row != None:
 		id=row[0]+1
 	
 	id=str(id)
-	conn.send(id.decode())
+	sendData=mAES.encrypt(id)
+	conn.send(sendData)
 
-	while 1:
-		#recv and decode the data 
-		data = conn.recv(1024).decode()
-		if not data:
-			break
-		#split the message based on the delimiter
-		id= data.split("|")[0]
-		currentTime= data.split("|")[1]
-		userPosition=data.split("|")[2]
-		cpuUsage= data.split("|")[3]
-		memoryUsage= data.split("|")[4]
-		diskUsage= data.split("|")[5]
+	#reset cipher
+	mAES= AESCipher(mPass)
+	#recv and decode the data 
+	recvData = conn.recv(1024)
+	data=mAES.decrypt(recvData)
+	if not data:
+		break
+	#split the message based on the delimiter
+	id= data.split("|")[0]
+	currentTime= data.split("|")[1]
+	userPosition=data.split("|")[2]
+	cpuUsage= data.split("|")[3]
+	memoryUsage= data.split("|")[4]
+	diskUsage= data.split("|")[5]
 
-		#printing for testing purposes
-		print("ID: %s" % id)
-		print("Current time: %s" % currentTime)
-		print("User Position: %s"% userPosition)
-		print("CPU Usage: %s" % cpuUsage)
-		print("Memory Usage: %s" % memoryUsage)
-		print("Disk Usage: %s" % diskUsage)
+	#printing for testing purposes
+	print("ID: %s" % id)
+	print("Current time: %s" % currentTime)
+	print("User Position: %s"% userPosition)
+	print("CPU Usage: %s" % cpuUsage)
+	print("Memory Usage: %s" % memoryUsage)
+	print("Disk Usage: %s" % diskUsage)
 
-		#this is where we will insert the data into our database
-		addData= ("INSERT INTO stats " "(id, ip, time, geolocation, cpu, memory, disk) "
-				   "VALUES (%s, %s, %s, %s, %s, %s, %s)")
+	#this is where we will insert the data into our database
+	addData= ("INSERT INTO stats " "(id, ip, time, geolocation, cpu, memory, disk) "
+			   "VALUES (%s, %s, %s, %s, %s, %s, %s)")
 
-		currentValues= (id, addr[0], currentTime, userPosition, float(cpuUsage), float(memoryUsage), float(diskUsage))
+	currentValues= (id, addr[0], currentTime, userPosition, float(cpuUsage), float(memoryUsage), float(diskUsage))
 
-		cur.execute(addData, currentValues)
-		db.commit()
-		cur.close()
+	cur.execute(addData, currentValues)
+	db.commit()
+	cur.close()
 	conn.close()
